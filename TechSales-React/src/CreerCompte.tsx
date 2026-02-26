a05fb1724c83803e311d3246a2a4f05c8a103e35
Diego Reinoso Breault <dreinoso.andres9@gmail.com>
2026-02-26 17:12:22 -0500

    Corrections de CreerCompte et Header Fait

--- TechSales-React/src/CreerCompte.tsx
+++ TechSales-React/src/CreerCompte.tsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router";
+import { HeaderPage } from "./main";
 import logo from "./assets/logo.png";
-import {useState} from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
 {
@@ -10,110 +10,125 @@ import { useNavigate } from "react-router";
     source OnChange : https://www.geeksforgeeks.org/reactjs/react-onchange-event/ https://stackoverflow.com/questions/71039088/what-is-onchange-e-setnamee-target-value-in-react-mean  */
 }
 
-
 export default function AfficherCreerCompte() {
   const navigate = useNavigate();
-  const[nom, setNom] = useState("");
-  const[prenom, setPrenom] = useState("");
-  const[email, setEmail] = useState("");
-  const[motDePasse, setMotDePasse] = useState("");
-  const[messageInscriptionMauvaise, setMessageInscriptionMauvaise] = useState(false);
+  const [BouttonDisabled, setBouttonDisabled] = useState(false);
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [email, setEmail] = useState("");
+  const [motDePasse, setMotDePasse] = useState("");
+  const [messageInscriptionMauvaise, setMessageInscriptionMauvaise] =
+    useState(false);
 
-function afficherMessageInscriptionMauvaise() {
-  setMessageInscriptionMauvaise(true);
-}
+  function afficherMessageInscriptionMauvaise() {
+    setMessageInscriptionMauvaise(true);
+  }
 
-function lireInfosInscription() { 
-  console.log("Nom:", nom);
-  console.log("Prénom:", prenom);
-  console.log("Email:", email);
-  console.log("Mot de Passe:", motDePasse);
-  setNom(nom);
-  setPrenom(prenom);
-  setEmail(email);
-  setMotDePasse(motDePasse);
-}
+  function lireInfosInscription() {
+    console.log("Nom:", nom);
+    console.log("Prénom:", prenom);
+    console.log("Email:", email);
+    console.log("Mot de Passe:", motDePasse);
+    setNom(nom);
+    setPrenom(prenom);
+    setEmail(email);
+    setMotDePasse(motDePasse);
+  }
 
-function CreationCompteBouttonClicked() {
-    const boutton = document.getElementById("CreationCompte") as HTMLButtonElement;
-    if (boutton && nom && prenom && email && motDePasse) {
-      boutton.disabled = true;
+  function CreationCompteBouttonClicked() {
+    if (nom && prenom && email && motDePasse) {
+      setBouttonDisabled(true);
       navigate("/Compte");
-    } else { setMessageInscriptionMauvaise(true); }
-}
+    } else {
+      setMessageInscriptionMauvaise(true);
+    }
+  }
 
   return (
-<div
-  className="rectangle"
-  style={{
-    backgroundColor: "#40365a",
-    backgroundImage:
-      "repeating-linear-gradient(25deg, rgba(48, 41, 49, 0.1) 0, rgba(214, 209, 209, 0.1) 5px, transparent 10px, transparent 20px)",
-    color: "white",
-  }}
->
+    <div
+      className="rectangle"
+      style={{
+        backgroundColor: "#40365a",
+      }}
+    >
       <main className="container-fluid text-center">
+        <HeaderPage />
         <div className="d-flex justify-content-center align-items-center vh-100">
           <div className="row">
-            <div className="col-12">
+            {/*Formulaire De Creation Compte*/}
+            <div className="col-12 my-3 p-5">
               <div
-                className="card shadow-lg p-5"
+                className="card shadow-lg p-3"
                 style={{ backgroundColor: "#f3efef", color: "white" }}
               >
-                <h2 className="card-title text-dark">
-                  <img src={logo} className="rounded mx-auto d-block" alt="logo" width={200} height={35}></img>
+                <h3 className="card-title text-dark">
+                  <img
+                    src={logo}
+                    className="rounded mx-auto d-block"
+                    alt="logo"
+                    width={200}
+                    height={35}
+                  ></img>
                   <br />
                   Créez votre Compte TechSales
-                </h2>
-                <div className="card shadow-lg m-4 mx-5 p-3">
+                </h3>
+                <div className="card shadow-lg m-4 mx-4 p-4">
                   <div className="form-group text-start">
-                    <label>Nom:</label>
                     <input
                       type="text"
                       className="form-control"
                       value={nom}
-                      placeholder="Entrez votre Nom"
+                      placeholder="Nom"
                       onChange={(e) => setNom(e.target.value)}
                     ></input>
                     <br />
                   </div>
                   <div className="form-group text-start">
-                    <label>Prénom:</label>
                     <input
                       type="text"
                       className="form-control"
                       value={prenom}
-                      placeholder="Entrez votre Prénom"
+                      placeholder="Prénom"
                       onChange={(e) => setPrenom(e.target.value)}
                     ></input>
                     <br />
                   </div>
                   <div className="form-group text-start">
-                    <label>Email:</label>
                     <input
                       type="text"
                       className="form-control"
                       value={email}
-                      placeholder="Entrez votre Email"
+                      placeholder="Email"
                       onChange={(e) => setEmail(e.target.value)}
                     ></input>
                     <br />
                   </div>
                   <div className="form-group text-start">
-                    <label>Mot de Passe:</label>
                     <input
                       type="password"
                       className="form-control"
                       value={motDePasse}
-                      placeholder="Entrez un mot de passe"
+                      placeholder="Mot de passe"
                       onChange={(e) => setMotDePasse(e.target.value)}
                     ></input>
                     <br />
                   </div>
-                  <button type="button" className="btn btn-dark" id="CreationCompte" onClick={() => {CreationCompteBouttonClicked(); lireInfosInscription();}}>
+                  <button
+                    type="button"
+                    className="btn btn-dark"
+                    disabled={BouttonDisabled}
+                    onClick={() => {
+                      CreationCompteBouttonClicked();
+                      lireInfosInscription();
+                    }}
+                  >
                     Créer Mon Compte
                   </button>
-                  {messageInscriptionMauvaise && <p className="text-danger">*Il manque des champs obligatoires*</p>}
+                  {messageInscriptionMauvaise && (
+                    <p className="text-danger">
+                      *Il manque des champs obligatoires*
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
--- TechSales-React/src/main.tsx
+++ TechSales-React/src/main.tsx
@@ -1,19 +1,69 @@
 import { StrictMode } from "react";
+import logo from "./assets/logo.png";
+import { Link } from "react-router";
+import user from "./assets/user.png";
+import panier from "./assets/panier.png";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {createBrowserRouter, RouterProvider} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { useNavigate } from "react-router";
 import "./index.css";
 import CreerCompte from "./CreerCompte.tsx";
 import Compte from "./Compte.tsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <CreerCompte /> },
-  { path: "/Compte", element: <Compte /> }
+  { path: "/Compte", element: <Compte /> },
 ]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
 );
+
+export const HeaderPage = () => {
+  return (
+    <main className="container-fluid text-center px-0">
+      <div className="row">
+        <div
+          className="col p-3"
+          style={{
+            backgroundColor: "#40365a",
+          }}
+        ></div>
+      </div>
+      <div className="row">
+        <div className="col bg-white p-4 d-flex justify-content-between align-items-center">
+          {/* Partie gauche */}
+          <div className="d-flex align-items-center gap-3">
+            <img src={logo} alt="logo" width={150} height={25} />
+            <Link to="/">
+              <button
+                type="button"
+                className="text-dark fw-bold btn btn-link ms-2"
+                style={{ fontSize: "12px" }}
+              >
+                Affichage Produits
+              </button>
+            </Link>
+          </div>
+
+          {/* Partie droite */}
+          <div className="d-flex align-items-center gap-3">
+            <Link to="/Compte">
+              <button type="button" className="btn p-0">
+                <img src={user} alt="user" width={30} height={30} />
+              </button>
+            </Link>
+
+            <Link to="/">
+              <button type="button" className="btn p-0">
+                <img src={panier} alt="panier" width={45} height={25} />
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
