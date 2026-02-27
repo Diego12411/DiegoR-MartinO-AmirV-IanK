fdd6ca276b4fcb1d5466aab34747f3ff8fc74d4b
Amirhossein Vakilian <6271394@bdeb.qc.ca>
2026-02-27 00:19:56 -0500

    routage dans main et background dans SeConnecter

---
+++ TechSales-React/src/SeConnecter.tsx
@@ -0,0 +1,11 @@
+export default function SeConnecter() {
+  return (
+    <div className="rectangle" style={{ backgroundColor: "#40365a" }}>
+      <main className="container-fluid text-center">
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <h1 className="text-white">Se connecter</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
--- TechSales-React/src/main.tsx
+++ TechSales-React/src/main.tsx
@@ -4,4 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+import { createBrowserRouter, RouterProvider } from "react-router";
+import SeConnecter from "./SeConnecter";
+
+// routage des pages de l'application
+const router = createBrowserRouter([
+  { path: "/seConnecter", element: <SeConnecter /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
