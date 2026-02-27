19d8939585a631913a60f4c46af5fc2bbc6edb70
Amirhossein Vakilian <6271394@bdeb.qc.ca>
2026-02-27 01:55:37 -0500

    modifier index.css

--- TechSales-React/src/SeConnecter.tsx
+++ TechSales-React/src/SeConnecter.tsx
@@ -3,7 +3,18 @@ export default function SeConnecter() {
     <div className="rectangle" style={{ backgroundColor: "#40365a" }}>
       <main className="container-fluid text-center">
         <div className="d-flex justify-content-center align-items-center vh-100">
-          <h1 className="text-white">Se connecter</h1>
+          <div className="row">
+            <div className="col-12 my-3 p-5">
+              <div
+                className="card shadow-lg p-3"
+                style={{ backgroundColor: "#f3efef" }}
+              >
+                <h3 className="card-title text-dark">
+                  Se connecter à TechSales
+                </h3>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
