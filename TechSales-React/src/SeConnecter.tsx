d46fa868312eb8ed9f5849ca77e42694baedcfd7
Amirhossein Vakilian <6271394@bdeb.qc.ca>
2026-02-27 02:31:28 -0500

    deplacer logo et texte

--- TechSales-React/src/SeConnecter.tsx
+++ TechSales-React/src/SeConnecter.tsx
@@ -1,3 +1,5 @@
+import logo from "./assets/logo.png";
+
 export default function SeConnecter() {
   return (
     <div className="rectangle" style={{ backgroundColor: "#40365a" }}>
@@ -9,9 +11,37 @@ export default function SeConnecter() {
                 className="card shadow-lg p-3"
                 style={{ backgroundColor: "#f3efef" }}
               >
-                <h3 className="card-title text-dark">
-                  Se connecter à TechSales
-                </h3>
+                <div className="text-start">
+                  <img src={logo} alt="logo" width={200} height={35} />
+
+                  <h2
+                    style={{
+                      marginTop: "20px",
+                      marginLeft: "10px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Se Connecter
+                  </h2>
+                </div>
+
+                <div className="form-group text-start">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Entrez votre Email"
+                  />
+                  <br />
+                </div>
+
+                <div className="form-group text-start">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Entrez votre mot de passe"
+                  />
+                  <br />
+                </div>
               </div>
             </div>
           </div>
