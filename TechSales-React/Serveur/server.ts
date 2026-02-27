bbb5d637980cabe7d68b417a8e7a777b1e104c97
martin-o-r <martin.ore.rodriguez@gmail.com>
2026-02-27 10:32:44 -0500

    Creation du pool pour la connexion MySQL

--- TechSales-React/Serveur/server.ts
+++ TechSales-React/Serveur/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
 
 const app = express();
 const PORT = 4000;
@@ -7,6 +8,14 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 
+// Create connection pool
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "martin",
+  password: "oracle",
+  database: "TechSales",
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
