229d0b048ddec72320b0bf664018f6ca73d68253
Amirhossein Vakilian <6271394@bdeb.qc.ca>
2026-03-11 19:15:05 -0400

    enlever les ancien code pour la page vide de compte

--- TechSales-React/src/Compte.tsx
+++ TechSales-React/src/Compte.tsx
@@ -77,13 +77,3 @@ export default function Compte() {
     </div>
   );
 }
-
-// export default function AfficherCompte() {
-//   return (
-//     <div className="container text-center">
-//       <h1>Bienvenue sur votre compte !</h1>
-//       <p>Voici les informations de votre compte :</p>
-//       {/* Affichez ici les informations du compte */}
-//     </div>
-//   );
-// }
