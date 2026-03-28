23ba8ba4e8911ff69fd5b5e44d183c4b77a2f0b9
Diego Reinoso <dreinoso.andres9@gmail.com>
2026-03-28 01:21:47 -0400

    Continuation frontend Panier

--- TechSales-React/src/Panier.tsx
+++ TechSales-React/src/Panier.tsx
@@ -8,13 +8,14 @@ export default function afficherPanier() {
   return (
     <main className="container-fluid text-center p-0">
       <HeaderComponent />
+      <p className="text-start my-5 mx-5 fw-bold">Panier</p>
       <div className="d-flex justify-content-center align-items-center my-5">
         <div className="row">
-          <div className="col-1">
+          <div className="col-12">
             <div
               className="card shadow-lg p-3"
               style={{ backgroundColor: "#f3efef", color: "white" }}
-            ></div>
+            >Panier</div>
           </div>
         </div>
       </div>
