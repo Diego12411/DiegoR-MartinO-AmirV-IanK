ceb981b872d18455a1f4cb96d57c0261c18e2ebf
Diego Reinoso Breault <dreinoso.andres9@gmail.com>
2026-03-30 21:10:48 -0400

    Panier presque fini

--- TechSales-React/src/Panier.tsx
+++ TechSales-React/src/Panier.tsx
@@ -1,22 +1,61 @@
 import { HeaderComponent } from "./main";
 import { FooterComponent } from "./main";
 import logo from "./assets/logo.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function afficherPanier() {
+interface Produit {
+  nom: string;
+  prix: number;
+  quantite: number;
+  image: string;
+  _id: string;
+}
+
+export function AfficherProduit({ produit }: { produit: Produit }) {
   return (
-    <main className="container-fluid text-center p-0">
+    <div className="card shadow-lg m-3 mx-5 p-3">
+      <div className="px-3 col bg-white p-1 d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-start gap-3">
+          img src={produit.image} {produit.nom}
+        </div>
+        <div className="d-flex align-items-center gap-3">{produit.prix}</div>
+        <div className="d-flex align-items-end gap-3">{produit.quantite}</div>
+      </div>
+    </div>
+  );
+}
+
+export default function afficherPanier() {
+  const [panier, setPanier] = useState<Produit[]>([]);
+  useEffect(() => {
+    // fetch ici
+  }, []);
+  return (
+    <main className="container-fluid p-0">
       <HeaderComponent />
-      <p className="text-start my-5 mx-5 fw-bold">Panier</p>
-      <div className="d-flex justify-content-center align-items-center my-5">
-        <div className="row">
-          <div className="col-12">
-            <div
-              className="card shadow-lg p-3"
-              style={{ backgroundColor: "#f3efef", color: "white" }}
-            >Panier</div>
+      <div className="my-3 text-start mx-5 p-1">
+        <strong>Panier</strong>
+      </div>
+      <div className="row px-5">
+        <div className="card shadow-lg me-5 p-3">
+          <div className="px-3 col bg-white p-1 d-flex justify-content-between align-items-center">
+            <div className="d-flex align-items-start gap-3">Produits</div>
+            <div className="d-flex align-items-center gap-3">Prix</div>
+            <div className="d-flex align-items-end gap-3">Quantité</div>
+            <div className="d-flex align-items-end gap-3">Sous-Total</div>
           </div>
+        </div>
+      </div>
+      {panier.map((produit) => (
+        <AfficherProduit key={produit._id} produit={produit} />
+      ))}
+      <div className="row px-5 my-2">
+        {" "}
+        <div className="col-lg">
+          <button className="mx-auto d-block ">
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </button>
         </div>
       </div>
       <FooterComponent />
