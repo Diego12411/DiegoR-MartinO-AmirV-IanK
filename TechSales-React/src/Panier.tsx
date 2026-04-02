046b93786d907d7102417124fd0b255c34c2ce9c
Diego Reinoso Breault <dreinoso.andres9@gmail.com>
2026-04-01 23:17:28 -0400

    Section sous total faite

--- TechSales-React/src/Panier.tsx
+++ TechSales-React/src/Panier.tsx
@@ -51,8 +51,82 @@ export default function afficherPanier() {
         <AfficherProduit key={produit._id} produit={produit} />
       ))}
       <div className="row">
-        <div className="col-2 p-2 d-flex justify-content-start"> <button className="p-3 w-100">Retourner sur le Magasin</button></div>
-        <div className="col-2 p-2 d-flex justify-content-end"> <button className="p-3 w-100">Passer la commande</button></div>
+        <div className="col-6 p-2 d-flex justify-content-start">
+          {" "}
+          <button className="p-3 mx-5 w-50 btn btn-outline-dark">
+            Retourner sur le Magasin
+          </button>
+        </div>
+        <div className="col-6 p-2 d-flex justify-content-end">
+          {" "}
+          <button className="p-3 mx-5 w-50 btn btn-outline-dark">
+            Passer la commande
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div
+            className="card shadow-lg mx-5 my-5 p-2"
+            style={{ border: "1.5px solid black", backgroundColor: "white" }}
+          >
+            <p
+              className="mx-3 my-1 p-1"
+              style={{ fontWeight: "500", color: "#212529" }}
+            >
+              Total du Panier
+            </p>
+            <div className="px-3 col bg-white d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-start gap-3">Sous-Total</div>
+            </div>
+            <div
+              style={{
+                borderTop: "1px solid black ",
+                opacity: "0.5",
+                marginTop: "8px", // Remonte la ligne vers le texte
+                marginBottom: "0px",
+                marginLeft: "12px",
+              }}
+            ></div>
+            <div className="px-3 col bg-white p-2 d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-start gap-3">Livraison</div>
+              <div className="d-flex align-items-end">Gratuit</div>
+            </div>
+            <div
+              style={{
+                borderTop: "1px solid black ",
+                opacity: "0.5",
+                marginTop: "0px", // Remonte la ligne vers le texte
+                marginBottom: "0px",
+                marginLeft: "12px",
+              }}
+            ></div>
+            <div className="px-3 col bg-white p-2 d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-start gap-3">Taxes</div>
+              <div className="d-flex align-items-end">sous-total*taxes</div>
+            </div>
+            <div
+              style={{
+                borderTop: "1px solid black ",
+                opacity: "0.5",
+                marginTop: "0px", // Remonte la ligne vers le texte
+                marginBottom: "0px",
+                marginLeft: "12px",
+              }}
+            ></div>
+            <div className="px-3 col bg-white p-2 d-flex justify-content-between align-items-center">
+              <div className="d-flex align-items-start gap-3">Total</div>
+              <div className="d-flex align-items-end">
+                sous-total + taxes + livraison
+              </div>
+            </div>
+            <div className="col bg-white d-flex justify-content-center">
+              <button className="p-3 my-5 btn btn-outline-dark">
+                Procéder au Paiement
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <FooterComponent />
     </main>
