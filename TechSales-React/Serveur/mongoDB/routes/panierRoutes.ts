2b76b29b12d7bda33995e5bbfd293d6b0c0abb27
martin-o-r <martin.ore.rodriguez@gmail.com>
2026-04-02 17:23:59 -0400

    Debut dev endpoints pour collection panier

--- TechSales-React/Serveur/mongoDB/routes/panierRoutes.ts
+++ TechSales-React/Serveur/mongoDB/routes/panierRoutes.ts
@@ -1,1 +1,9 @@
 import { Router } from "react-router";
+import {
+  createCart,
+  getCartFromUser,
+  addNewItemToCart,
+  removeItemFromCart,
+  updateItemQuantity,
+  clearCart,
+} from "../controllers/panierController.js";
