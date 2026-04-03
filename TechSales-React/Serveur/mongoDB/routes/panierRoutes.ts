db21fb628f9e188a326e0a8490a137f7b4f617d9
martin-o-r <martin.ore.rodriguez@gmail.com>
2026-04-03 09:51:25 -0400

    Ajout du app.listen() dans le fichier server.ts Afin de permettre lla communication du frontend aux endpoints

--- TechSales-React/Serveur/mongoDB/routes/panierRoutes.ts
+++ TechSales-React/Serveur/mongoDB/routes/panierRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from "react-router";
+import { Router, Request, Response } from "express";
 import {
   createCart,
   getCartFromUser,
@@ -7,3 +7,15 @@ import {
   updateItemQuantity,
   clearCart,
 } from "../controllers/panierController.js";
+import { getPaniers } from "../db/mongo.js";
+import { Panier } from "../models/panier.js";
+import { ObjectId } from "mongodb";
+
+const router = Router();
+
+// Test des endpoins avec le serveur
+router.get("/testTest", async (req: Request, res: Response) => {
+  res.send("Endpond test reussi!!");
+});
+
+export default router;
--- TechSales-React/Serveur/mongoDB/server.ts
+++ TechSales-React/Serveur/mongoDB/server.ts
@@ -1,6 +1,13 @@
+import express from "express";
+import panierRoutes from "./routes/panierRoutes.js";
 import { config } from "dotenv";
 import { connectToMongo, getProduits } from "./db/mongo.js";
-import { createProduit, getProduitById, updateProduit, deleteProduitById } from "./controllers/produitController.js";
+import {
+  createProduit,
+  getProduitById,
+  updateProduit,
+  deleteProduitById,
+} from "./controllers/produitController.js";
 
 config();
 
@@ -11,3 +18,11 @@ if (!uri) {
 console.log("Connexion a MongoDB reussi!! :)");
 
 await connectToMongo(uri);
+
+const app = express();
+app.use(express.json());
+
+// Ajouter les routes dans cette section
+app.use("/paniers", panierRoutes);
+
+app.listen(process.env.PORT);
