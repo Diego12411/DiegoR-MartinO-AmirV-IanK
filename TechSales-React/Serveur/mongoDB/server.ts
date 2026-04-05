c8dc027eb197b27ab816eb68b69ebf193c69f9e8
Amirhossein Vakilian <6271394@bdeb.qc.ca>
2026-04-04 23:49:30 -0400

    changer la format de listen

--- TechSales-React/Serveur/mongoDB/server.ts
+++ TechSales-React/Serveur/mongoDB/server.ts
@@ -15,9 +15,10 @@ const uri = process.env.MONGODB_URI;
 if (!uri) {
   throw new Error("MONGODB_URI is not defined");
 }
-console.log("Connexion a MongoDB reussi!! :)");
 
 await connectToMongo(uri);
+// Tester la connexion apres l'etablissement de la connection
+console.log("Connexion à MongoDB réussie !! :)");
 
 const app = express();
 app.use(express.json());
@@ -25,7 +26,18 @@ app.use(express.json());
 // Ajouter les routes dans cette section ci-dessous
 app.use("/paniers", panierRouter);
 
-app.listen(process.env.PORT);
+// pour tester le serveur
+app.get("/test", (_req, res) => {
+  res.json({ message: "Serveur fonctionne !" });
+});
+
+// Je l'ai changé, car c'est risqué comme avant. Port peut etre undifined.
+// Avant : app.listen(process.env.PORT);
+// Listen sur le port défini dans le fichier .env ou 4000 par défaut, et affiche un message de confirmation dans la console une fois que le serveur est démarré.
+const PORT = Number(process.env.PORT) || 4000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
 
 /**
  * Pour tester vos endpoints :
