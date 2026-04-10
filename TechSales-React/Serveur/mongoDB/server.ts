8a95b0845bede81707861b8514b6e21e78db189b
Martin Ore Rodriguez <martin.ore.rodriguez@gmail.com>
2026-04-10 10:37:23 -0400

    Feature/24 backend routes panier (#89)
    * Debut dev endpoints pour collection panier
    
    * Correction dans le methode updateItemQuantity() dans le fichier panierController.ts
    Dans le $set, on doit faisait reference a producId, mais il fallait plutot faire reference a .quanity.
    
    * Ajout du app.listen() dans le fichier server.ts
    Afin de permettre lla communication du frontend aux endpoints
    
    * Ajout de commentaire dans le fichier server.ts
    Ajout de commentaire expliquant comment utiliser les endpoints avec la nouvelle structure utilisee avec mongo
    
    * Traduction des methodes mongo en francais
    
    * Correction des fautes dans panierController
    Ajout endpoint POST dans panierRouter.ts qui permet de rajouter un panier dans la base de donnees. Aussi, correction des champs entres dans le panierController.ts
    
    * Ajout des endpoints dans fichier panierRouter.ts
    develeoppement des autres endpoints
    
    * Developpement des endpoints
    Continuation du developpement des endspoints avec ajout des verifications necessaires avant dappeler les methodes du controller.
    
    * Correction dans panierRouter
    Comprehension du _id? dans les interfaces. Finalement, on n'avait pas besoin de rajouter un _id? dans l'interface panier, car on utilise le produitId pour retrouver des items par apres dans les routes aka endpoints
    
    * Correction de code doublon dans le fichier server.ts
    
    * Correction d'erreur
    Dans le fichier server.ts, retrait des doublons et nettoyage. Ce qui est gardé, c'est ce qui est nécessaire seulement. Dans le fichier itemPanier.ts, retrait de _id? dans l'interface itemPanier, car on utilise produitId dans le controller pour manipuler les itemPanier et non son _id.

--- TechSales-React/Serveur/mongoDB/server.ts
+++ TechSales-React/Serveur/mongoDB/server.ts
@@ -2,13 +2,7 @@ import express from "express";
 import panierRouter from "./routes/panierRouter.js";
 import utilisateurRouter from "./routes/utilisateurRouter.js";
 import { config } from "dotenv";
-import { connectToMongo, getProduits } from "./db/mongo.js";
-import {
-  createProduit,
-  getProduitById,
-  updateProduit,
-  deleteProduitById,
-} from "./controllers/produitController.js";
+import { connectToMongo } from "./db/mongo.js";
 
 config();
 
@@ -18,8 +12,6 @@ if (!uri) {
 }
 
 await connectToMongo(uri);
-// Tester la connexion apres l'etablissement de la connection
-console.log("Connexion à MongoDB réussie !! :)");
 
 const app = express();
 app.use(express.json());
@@ -28,13 +20,8 @@ app.use(express.json());
 app.use("/paniers", panierRouter);
 app.use("/utilisateurs", utilisateurRouter);
 
-// Je l'ai changé, car c'est risqué comme avant. Port peut etre undifined.
-// Avant : app.listen(process.env.PORT);
-// Listen sur le port défini dans le fichier .env ou 4000 par défaut, et affiche un message de confirmation dans la console une fois que le serveur est démarré.
-const PORT = Number(process.env.PORT) || 4000;
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
+// listener
+app.listen(process.env.PORT);
 
 /**
  * Pour tester vos endpoints :
@@ -42,6 +29,6 @@ app.listen(PORT, () => {
  * -> Demarrez le serveur avec commande : npm run dev
  * -> Pour acceder a vos endpoints (voici un exemple avec la collection panier) : http://localhost:4000/paniers/testTest
  *  -> le port c'est 4000, definie dans vos fichier .env
- *  -> chemin "/paniers" est defini dans server.ts a la ligne #26
+ *  -> chemin "/paniers" est defini dans server.ts a la ligne #20
  *  -> endpoint "/testTest" est definie dans le fichier routes/panierRouter.ts
  */
