ed444f087567d963b74f71137b7df2c63a2d3ec3
IanVsGitUser <vitruviusk@gmail.com>
2026-05-08 11:40:19 -0400

    Modif. page panier/logo footer redirige vers page main

--- TechSales-React/src/Panier.tsx
+++ TechSales-React/src/Panier.tsx
@@ -2,7 +2,9 @@ import { HeaderComponent } from "./main";
 import { FooterComponent } from "./main";
 import logo from "./assets/logo.png";
 import "./Panier.css";
+import { Link } from "react-router";
 import { useState, useEffect } from "react";
+import sansImage from "./assets/ProduitSansImage.png";
 import { useNavigate } from "react-router-dom";
 
 interface Produit {
@@ -12,19 +14,55 @@ interface Produit {
   image: string;
   _id: string;
 }
+
+function RetirerProduit(produit: Produit) {
+  // A travailler
+}
+
 export function AfficherProduit({ produit }: { produit: Produit }) {
   return (
-    <div className="row px-5 my-3">
-      <div className="card shadow-lg me-5 p-3 py-4">
-        <div className="px-3 col-12 bg-white p-1 d-flex justify-content-between align-items-center">
-          <div className="d-flex align-items-start col-4">{produit.nom}</div>
-          <div className="d-flex align-items-center col-3">
+    <div className="row px-5 my-3 justify-content-left">
+      <div className="card shadow-lg me-5 p-3 py-3">
+        <div className="col-12 bg-white p-1 d-flex justify-content-left align-items-center">
+          <div className="d-flex justify-content-left align-items-center col-5">
+            {/*Image du produit*/}
+            <img
+              className="card shadow border-dark bg-light me-3"
+              src={produit.image || sansImage}
+              alt={produit.nom}
+              style={{
+                minWidth: "50px",
+                maxWidth: "50px",
+                minHeight: "50px",
+                maxHeight: "50px",
+                objectFit: "contain",
+              }}
+            ></img>
+            {/*Ramene a la page details du produit associé lorsqu'on clique le nom du produit*/}
+            <Link
+              to={`../produits/detailsProduit/${produit._id}`}
+              className="text-secondary text-decoration-none"
+            >
+              <div>{produit.nom}</div>
+            </Link>
+          </div>
+          <div className="d-flex justify-content-left align-items-center col-2">
             {produit.prix} $
           </div>
-          <div className="d-flex align-items-end col-4">{produit.quantite}</div>
-          <div className="d-flex align-items-end col-1">
+          <div className="d-flex justify-content-left align-items-center col-2">
+            {produit.quantite}
+          </div>
+          <div className="d-flex justify-content-left align-items-center col-2">
             {produit.prix * produit.quantite} $
           </div>
+          {/*Bouton retirer le produit du panier*/}
+          <button
+            className="d-flex justify-content-left align-items-center col-1 btn btn-outline-dark btn-panier"
+            style={{ minWidth: "75px", maxWidth: "75px" }}
+            onClick={() => RetirerProduit(produit)}
+          >
+            Retirer
+          </button>
         </div>
       </div>
     </div>
@@ -34,14 +72,36 @@ export function AfficherProduit({ produit }: { produit: Produit }) {
 export default function afficherPanier() {
   const navigate = useNavigate();
   const [panier, setPanier] = useState<Produit[]>([
-    { _id: "1", nom: "Produit Test", prix: 99.99, quantite: 2, image: "" },
-    { _id: "2", nom: "Deuxième Produit", prix: 49.99, quantite: 1, image: "" },
+    {
+      _id: "1",
+      nom: "Produit Test",
+      prix: 99.99,
+      quantite: 2,
+      image:
+        "https://dlcdnwebimgs.asus.com/gain/3C38EBCB-420C-438B-B02F-072F4A9E47DB",
+    },
+    {
+      _id: "2",
+      nom: "Deuxième Produit",
+      prix: 49.99,
+      quantite: 4,
+      image:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp14-spacegray-select-202310",
+    },
+    {
+      _id: "3",
+      nom: "Ordinateur",
+      prix: 1299.99,
+      quantite: 1,
+      image:
+        "https://www.lg.com/content/dam/channel/wcms/ca_en/images/laptops/gram/17z90sp-g-aa75a9/DZ-02.jpg",
+    },
   ]);
   const [messageBouttonAcheter, setMessageBouttonAcheter] = useState("");
   const livraison = 0;
   const sousTotal = panier.reduce(
     (total, produit) => total + produit.prix * produit.quantite,
-    0,
+    0
   );
   const taxes = sousTotal * 0.15;
   const total = taxes + sousTotal + livraison;
@@ -60,25 +120,43 @@ export default function afficherPanier() {
       <div className="my-3 text-start mx-5 p-1">
         <strong>Panier</strong>
       </div>
-      <div className="row px-5">
+      <div className="row px-5 justify-content-left">
         <div className="card shadow-lg me-5 px-3 py-2">
-          <div className="px-3 col-12 bg-white p-1 d-flex justify-content-between align-items-center">
-            <div className="d-flex align-items-start col-4">Produits</div>
-            <div className="d-flex align-items-center col-3">Prix</div>
-            <div className="d-flex align-items-end col-4">Quantité</div>
-            <div className="d-flex align-items-end col-1">Sous-Total</div>
+          <div className="col-12 bg-white p-1 d-flex justify-content-left align-items-center">
+            <div className="d-flex justify-content-left align-items-center col-5">
+              Produits
+            </div>
+            <div className="d-flex justify-content-left align-items-center col-2">
+              Prix
+            </div>
+            <div className="d-flex justify-content-left align-items-center col-2">
+              Quantité
+            </div>
+            <div className="d-flex justify-content-left align-items-center col-2">
+              Sous-Total
+            </div>
+            <div className="d-flex justify-content-left align-items-center col-1"></div>
           </div>
         </div>
       </div>
-      {panier.map((produit) => (
-        <AfficherProduit key={produit._id} produit={produit} />
-      ))}
+      <div
+        className="overflow-auto overflow-x-hidden"
+        style={{ height: "300px", scrollBehavior: "smooth" }}
+      >
+        {panier.map((produit) => (
+          <div key={produit._id}>
+            <AfficherProduit key={produit._id} produit={produit} />
+          </div>
+        ))}
+      </div>
       <div className="row">
         <div className="col-6 p-2 d-flex justify-content-start">
           {" "}
-          <button className="p-3 mx-5 w-50 btn btn-outline-dark">
-            Ajouter d'autres produits au panier
-          </button>
+          <Link to="/">
+            <button className="p-3 mx-5 w-50 btn btn-outline-dark">
+              Ajouter d'autres produits au panier
+            </button>
+          </Link>
         </div>
         <div className="col-6 p-2 d-flex justify-content-end">
           {" "}
